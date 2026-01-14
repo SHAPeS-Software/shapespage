8df3f5d0f86733ba1755bac1b6c3d5b43a8a82a3
SHAPeS-Software <noah.g.270stx@gmail.com>
2026-01-13 21:39:16 -0600

    Updated package and post build script to fix deploy issue

--- scripts/build.ts
+++ scripts/build.ts
@@ -13,6 +13,10 @@ const fontData = readFileSync(
 
 const POSTS_DIR = join(process.cwd(), 'public', 'posts')
 const DIST_DIR = join(process.cwd(), 'dist')
+if (!existsSync(DIST_DIR)) {
+  mkdirSync(DIST_DIR, { recursive: true });
+  console.log('Created dist directory');
+}
 
 interface Frontmatter {
     title: string
