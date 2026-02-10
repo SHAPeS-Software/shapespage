75f9aeb02020096545be84c81473dc7562529cf6
SHAPeS-Software <noah.g.270stx@gmail.com>
2026-02-10 16:02:43 -0600

    This is getting stupid, added a trailing slash in astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ import { defineConfig } from "astro/config";
 // https://astro.build/config
 export default defineConfig({
   site: "https://shapes-soft.github.io",
-  base: "/shapespage",
+  base: "/shapespage/",
 });
